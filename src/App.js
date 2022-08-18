73d762b78fe8658dff6ad266da5207911fffd822
Professor-promise <professorpromise1000@gmail.com>
2022-08-18 21:03:21 +0100

    Random Users with RTK Cuery

--- src/App.js
+++ src/App.js
@@ -1,57 +1,119 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { useGetUsersQuery } from './app/services/users';
+import {
+  FaEnvelopeOpen,
+  FaUser,
+  FaCalendarTimes,
+  FaMap,
+  FaPhone,
+  FaLock,
+} from 'react-icons/fa';
 
 function App() {
+  const [person, setPerson] = useState([]);
+  const [value, setValue] = useState('Random Person');
+  const [title, setTitle] = useState('Name');
+
+  const { data, isLoading, refetch } = useGetUsersQuery();
+  const defaultImage = 'https://randomuser.me/api/portraits/men/75.jpg';
+
+  useEffect(() => {
+    if (data) {
+      const randomUser = data.results[0];
+      const {
+        email,
+        phone,
+        picture: { large: image },
+        login: { password },
+        name: { first, last },
+        dob: { age },
+        location: {
+          street: { number, name },
+        },
+      } = randomUser;
+      const newPerson = {
+        email,
+        phone,
+        image,
+        password,
+        age,
+        street: `${number} ${name}`,
+        name: `${first} ${last}`,
+      };
+      setPerson(newPerson);
+      setTitle('Name');
+      setValue(newPerson.name);
+    }
+  }, [data]);
+
+  const handleValue = e => {
+    e.preventDefault();
+
+    if (e.target.classList.contains('icon')) {
+      const newValue = e.target.dataset.label;
+      setTitle(newValue);
+      setValue(person[newValue]);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <main>
+      <div className='block bcg-black'></div>
+      <div className='block'>
+        <div className='container'>
+          <img
+            src={person?.image || defaultImage}
+            alt='random user'
+            className='user-img'
+          />
+          <p className='user-title'>My {title}</p>
+          <p className='user-value'>{value}</p>
+          <div className='values-list'>
+            <button
+              data-label='name'
+              className='icon'
+              onMouseOver={handleValue}
+            >
+              <FaUser />
+            </button>
+            <button
+              data-label='email'
+              className='icon'
+              onMouseOver={handleValue}
+            >
+              <FaEnvelopeOpen />
+            </button>
+            <button data-label='age' className='icon' onMouseOver={handleValue}>
+              <FaCalendarTimes />
+            </button>
+            <button
+              data-label='street'
+              className='icon'
+              onMouseOver={handleValue}
+            >
+              <FaMap />
+            </button>
+            <button
+              data-label='phone'
+              className='icon'
+              onMouseOver={handleValue}
+            >
+              <FaPhone />
+            </button>
+            <button
+              data-label='password'
+              className='icon'
+              onMouseOver={handleValue}
+            >
+              <FaLock />
+            </button>
+          </div>
+          <button className='btn' type='button' onClick={() => refetch()}>
+            {isLoading ? 'Loading...' : 'Random User'}
+          </button>
+        </div>
+      </div>
+    </main>
   );
 }
 
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { usersApi } from './services/users';
+import { setupListeners } from '@reduxjs/toolkit/dist/query';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    [usersApi.reducerPath]: usersApi.reducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware().concat(usersApi.middleware),
 });
+
+setupListeners(store.dispatch);
